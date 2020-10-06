8e51870e8924b3b612f25ef2c67899c01bb1c618
hejiayi <1506004461@qq.com>
2020-10-06 16:04:07 +0800

    撤回版本 再次提交属性

--- src/com/jy/testGit.java
+++ src/com/jy/testGit.java
@@ -6,13 +6,9 @@ public class testGit {
         System.out.println("噶一测试版本第一次");
         hejiayi he = new hejiayi("贺甲一",18);
         System.out.println("名字："+he.getName()+",年龄:"+he.getAge());
-        System.out.println("噶一测试版本第二次");
+        System.out.println("噶一测试版本第二次 再次");
         he.setName("噶一");
         System.out.println("名字："+he.getName());
-        System.out.println("噶一测试版本第三次");
-        he.setName("社会你一哥哥");
-        System.out.println("名字："+he.getName());
-
 
     }
 }
