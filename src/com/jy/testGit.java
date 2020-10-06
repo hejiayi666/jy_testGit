6358182370142cb2b08341ad947b5355d268d152
hejiayi <1506004461@qq.com>
2020-10-06 15:46:46 +0800

    提交贺甲一属性第二次

--- src/com/jy/testGit.java
+++ src/com/jy/testGit.java
@@ -3,8 +3,12 @@ package com.jy;
 public class testGit {
 
     public static void main(String[] args) {
-        System.out.println("一哥测试NO。1");
+        System.out.println("噶一测试版本第一次");
         hejiayi he = new hejiayi("贺甲一",18);
         System.out.println("名字："+he.getName()+",年龄:"+he.getAge());
+        System.out.println("噶一测试版本第二次");
+        he.setName("噶一");
+        System.out.println("名字："+he.getName());
+
     }
 }
