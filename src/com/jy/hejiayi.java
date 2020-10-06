75a2651c53767859b9dde70e184881801f0687e8
hejiayi <1506004461@qq.com>
2020-10-06 15:39:48 +0800

    提交贺甲一属性

---
+++ src/com/jy/hejiayi.java
@@ -0,0 +1,30 @@
+package com.jy;
+
+public class hejiayi {
+
+    private String  name;
+    private int age;
+
+    public hejiayi(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
--- src/com/jy/testGit.java
+++ src/com/jy/testGit.java
@@ -1,5 +1,10 @@
 package com.jy;
 
 public class testGit {
-    
+
+    public static void main(String[] args) {
+        System.out.println("一哥测试NO。1");
+        hejiayi he = new hejiayi("贺甲一",18);
+        System.out.println("名字："+he.getName()+",年龄:"+he.getAge());
+    }
 }
